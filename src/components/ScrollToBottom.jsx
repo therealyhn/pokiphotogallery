65b6dff6b6542c3289095a4cdf756e0253912890
Jovan Ljusic <jovanljusicum@outlook.com>
2025-09-26 17:35:45 +0200

    sc

--- src/components/ScrollToBottom.jsx
+++ src/components/ScrollToBottom.jsx
@@ -14,7 +14,7 @@ export default function ScrollToBottom() {
     return (
         <button
             onClick={scrollToBottom}
-            className="fixed top-4 md:right-4 right-2 z-50 bg-main hover:bg-tr transition duration-300 text-black px-6 py-2 rounded-full shadow-lg flex items-center"
+            className="fixed top-4 left-4 z-50 bg-main hover:bg-tr transition duration-300 text-black px-6 py-2 rounded-full shadow-lg flex items-center"
         >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 9l-7 7-7-7"/></svg>
         </button>
